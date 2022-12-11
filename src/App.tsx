e5c270b3a25c78609bf5a2791242f95ac14e0243
Kaif Siddiqui <mdkaifprofession@gmail.com>
2022-12-11 11:45:30 +0530

    Handle clicks on same tile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import './App.css'
 
 function App() {
@@ -9,22 +10,56 @@ function App() {
       [4, 3, 1, 3]
     ]
   )
+  
+  type previousSelectionState = {
+    rowIndex: number | undefined;
+    colmIndex: number | undefined;
+  }
+
+  const nullObject = {rowIndex: undefined, colmIndex: undefined}
+  const [previousSelection, setPreviousSelection] = useState<previousSelectionState>(nullObject)
 
   const [revealedGrid, setRevealedGrid] = useState(
     new Array(grid.length).fill("").map(row => (new Array(4).fill(false)))
   )
 
   const handleClick = (rowIndex: number, colmIndex: number) =>{
-    // if user clicks for the first time only reveal the card
-    const newRevealedGrid = [...revealedGrid]
-    newRevealedGrid[rowIndex][colmIndex] = true
-    setRevealedGrid(newRevealedGrid)
     
+    // when users clicks the card, update the grid
+
+    if(revealedGrid[rowIndex][colmIndex] === true)return 
+    const revealedGridCopy = [...revealedGrid]
+    revealedGridCopy[rowIndex][colmIndex] = true
+    setRevealedGrid(revealedGridCopy)
+    
+
+
+    // if the user selects for the first time, update the previous Selection state
+    if(previousSelection.rowIndex == undefined || previousSelection.colmIndex == undefined){
+      setPreviousSelection(prev => ({...prev, rowIndex: rowIndex, colmIndex: colmIndex}))
+      return
+    }
+
+    // if previous selection does not match out current Selection, remove the previous and the current change
+    // made on the grid after a delay of 1 second.
+    if(grid[previousSelection.rowIndex][previousSelection.colmIndex] !== grid[rowIndex][colmIndex]){
+      // if the card do not match, change the both the cards after delay of 1 second
+      setTimeout(() => {
+        const revealedGridCopy = [...revealedGrid]
+        if(previousSelection.rowIndex === undefined || previousSelection.colmIndex === undefined) return 
+        revealedGridCopy[previousSelection.rowIndex][previousSelection.colmIndex] = false
+        revealedGridCopy[rowIndex][colmIndex] = false
+        setRevealedGrid(revealedGridCopy)
+        setPreviousSelection({rowIndex: undefined, colmIndex: undefined})
+      }, (1000));
+    }
+    // if the grid matches, update the previous selection state to be none
+    else{
+      setPreviousSelection({rowIndex: undefined, colmIndex: undefined})
+    }
+
   }
 
-  // check if the user has clicked 
-  // if the first card number matches the second card number : do not change the grid
-  // if the card do not match, change the both the cards after delay of 1 second
   
 
   return (
